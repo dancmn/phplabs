5b2eb5064ef614e9f58a1085edaca7f8462fda01
dancmn <bakhmud_kh@mail.ru>
2024-02-06 15:24:02 +0300

    lab1

---
+++ lab1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <img src="../img/mos_politech_logo.png" alt="" class='logo'>
+            <p>Домашняя работа: Hello, World!</p>
+        </div>
+    </header>
+
+    <main>
+        <?php
+            echo 'Hello World!'
+        ?>
+    </main>
+
+    <footer>
+        <div class="wrapper">
+            <p>Задание для самостоятельно работы</p>
+        </div>
+    </footer>
+</body>
+</html>
