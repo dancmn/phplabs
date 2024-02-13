fe774204d543665e4725c2385e16147acb560be2
dancmn <bakhmud_kh@mail.ru>
2024-02-13 15:49:23 +0300

    lab3

---
+++ lab3/back.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print_r(get_headers('https://www.gettyimages.com/photos/kyrie-irving?assettype=image&family=editorial&page=3&phrase=kyrie%20irving&sort=newest'));
+    ?>
+    <a href="index.html">Переход на первую страницу</a>
+</body>
+</html>
